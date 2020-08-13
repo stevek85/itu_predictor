e9ed2757bf61baeda18f6fde71913db4bb84348b
Stephen Kyriacou <stephenkyriacou@Stephens-MacBook-Pro.local>
2020-08-13 16:57:24 +0100

    Added error handling

--- routes.py
+++ routes.py
@@ -14,19 +14,27 @@ def home():
 @app.route("/create", methods=['POST'])
 def create():
     if request.form:
-        book = Book(title=request.form.get("title"))
-        db.session.add(book)
-        db.session.commit()
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
     return redirect("/")
 
 
 @app.route("/update", methods=["POST"])
 def update():
-    newtitle = request.form.get("newtitle")
-    oldtitle = request.form.get("oldtitle")
-    book = Book.query.filter_by(title=oldtitle).first()
-    book.title = newtitle
-    db.session.commit()
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
     return redirect("/")
 
 
