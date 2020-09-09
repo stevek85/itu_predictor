e4d03fafc3105e25c71ac75aed7ef0c10b63b008
Stephen Kyriacou <stephenkyriacou@Stephens-MacBook-Pro.local>
2020-09-09 13:19:13 +0300

    simple api

--- routes.py
+++ routes.py
@@ -3,7 +3,9 @@ from flask import request
 from flask import redirect
 from models import Book
 from config import app, db
-
+from utils import process_url_params
+from logic import model_set
+import json
 
 @app.route("/")
 def home():
@@ -11,37 +13,8 @@ def home():
     return render_template("home.html", books=books)
 
 
-@app.route("/create", methods=['POST'])
-def create():
-    if request.form:
-        try:
-            book = Book(title=request.form.get("title"))
-            db.session.add(book)
-            db.session.commit()
-        except Exception as e:
-            print("Failed to add book")
-            print(e)
-    return redirect("/")
-
-
-@app.route("/update", methods=["POST"])
-def update():
-    try:
-        newtitle = request.form.get("newtitle")
-        oldtitle = request.form.get("oldtitle")
-        book = Book.query.filter_by(title=oldtitle).first()
-        book.title = newtitle
-        db.session.commit()
-    except Exception as e:
-        print("Couldn't update book title")
-        print(e)
-    return redirect("/")
-
-
-@app.route("/delete", methods=["POST"])
-def delete():
-    title = request.form.get("title")
-    book = Book.query.filter_by(title=title).first()
-    db.session.delete(book)
-    db.session.commit()
-    return redirect("/")
+@app.route("/api/<model>/", methods=["GET"])
+def api_get_model(model):
+    query_dict = process_url_params(request.args)
+    model = model_set[model](query_dict)
+    return json.dumps({'prediction': model.get_prediction()})
